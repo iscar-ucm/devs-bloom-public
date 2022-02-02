1640fff70b1651565d469d847a1e06b99aa16079
José L. Risco-Martín <jlrisco@ucm.es>
2022-02-02 19:49:31 +0100

    Primera prueba del Fog.

--- fog/fog.py
+++ fog/fog.py
@@ -53,34 +53,85 @@ class TmpGenerator(Atomic):
 
 
 class FogServer(Atomic):
-    """ A model for the fog server."""
+    """Servidor Fog."""
 
-    def __init__(self, name, n_samples):
+    def __init__(self, name, n_samples=100):
+        """Función de inicialización de atributos."""
         super().__init__(name)
         self.n_samples = n_samples
+        # Puertos de ejemplo, sobre los que se hace detección de outliers:
         self.i_sensor_temp_01 = Port(Event, "i_sensor_temp_01")
         self.add_in_port(self.i_sensor_temp_01)
         self.o_sensor_temp_01_raw = Port(pd.DataFrame, "o_sensor_temp_01_raw")
         self.add_out_port(self.o_sensor_temp_01_raw)
         self.o_sensor_temp_01_new = Port(pd.DataFrame, "o_sensor_temp_01_new")
         self.add_out_port(self.o_sensor_temp_01_new)
+        # Puerto con los datos de barco y bloom fusionados.
+        # Cada n_samples se agrupan los datos en un dataframe y se lanzan al
+        # Cloud.
+        self.i_fusion_01 = Port(Event, "i_fusion_01")
+        self.o_fusion_01 = Port(pd.DataFrame, "o_fusion_01")
+        self.add_in_port(self.i_fusion_01)
+        self.add_out_port(self.o_fusion_01)
 
     def initialize(self):
-        self.sensor_temp_01_raw = pd.DataFrame(columns=["id","timestamp","value"])
-        self.sensor_temp_01_new = pd.DataFrame(columns=["id","timestamp","value"])
-        self.counter = 0
+        """Incialización de la simulación DEVS."""
+        self.sensor_temp_01_raw = pd.DataFrame(columns=["id",
+                                                        "timestamp",
+                                                        "value"])
+        self.sensor_temp_01_new = pd.DataFrame(columns=["id",
+                                                        "timestamp",
+                                                        "value"])
+        self.fusion_01_raw = pd.DataFrame(columns=["id",
+                                                   "source",
+                                                   "datetime",
+                                                   "payload"])
+        # TODO: Esto lo tengo que pasar a otro modelo atómico, de hecho:
+        # 1.- FogServer pasaría a ser FogData.
+        # 2.- Nuevo modelo atómico FogDb que almacene datos y detecte outliers.
+        # 3.- Nuemo modelo acoplado FogServer que incluya estos dos.
+        self.fusion_01_db = pd.DataFrame(columns=["id",
+                                                  "source",
+                                                  "datetime",
+                                                  "payload"])
+        self.sensor_temp_01_counter = 0
+        self.fusion_01_counter = 0
         self.passivate()
 
     def exit(self):
-        pass
+        """Función de salida de la simulación."""
+        self.fusion_01_db.to_csv("data/FogData.csv")
 
     def lambdaf(self):
-        self.o_sensor_temp_01_raw.add(self.sensor_temp_01_raw)
-        self.o_sensor_temp_01_new.add(self.sensor_temp_01_new)
+        """Función DEVS de salida."""
+        if (self.sensor_temp_01_counter == self.n_samples):
+            self.o_sensor_temp_01_raw.add(self.sensor_temp_01_raw)
+            self.o_sensor_temp_01_new.add(self.sensor_temp_01_new)
 
     def deltext(self, e):
+        """Función DEVS de transición externa."""
         self.continuef(e)
-        if (self.i_sensor_temp_01.empty() == False):
+
+        # Procesamos el puerto i_fusion_01:
+        if (self.i_fusion_01.empty() is False):
+            msg = self.i_fusion_01.get()
+            items = msg.payload.items()
+            columns = ["id", "source", "datetime", "payload"]
+            content = [msg.id, msg.source, msg.timestamp, msg.payload]
+            for item in items:
+                columns.append(item[0])
+                content.append(item[1])
+            new_data = pd.DataFrame(content, columns)
+            self.fusion_01_raw = self.fusion_01_raw.append(new_data.T,
+                                                           ignore_index=True)
+            self.fusion_01_db = self.fusion_01_db.append(new_data.T,
+                                                         ignore_index=True)
+            self.fusion_01_counter += 1
+            if(self.fusion_01_counter == self.n_samples):
+                # De momento, no comprobamos outliers.
+                super().activate()
+
+        if (self.i_sensor_temp_01.empty() is False):
             current_input = self.i_sensor_temp_01.get()
             id = current_input.id
             timestamp = current_input.timestamp
@@ -88,36 +139,51 @@ class FogServer(Atomic):
             values = {"id": id, "timestamp": timestamp, "value": value}
             self.sensor_temp_01_raw = self.sensor_temp_01_raw.append(
                 values, ignore_index=True)
-            self.counter = self.counter + 1
-            if (self.counter == self.n_samples):
+            self.sensor_temp_01_counter = self.sensor_temp_01_counter + 1
+            if (self.sensor_temp_01_counter == self.n_samples):
                 lof = nb.LocalOutlierFactor()
                 wrong_values = lof.fit_predict(self.sensor_temp_01_raw[["value"]])
                 outlier_index = np.where(wrong_values == -1)
                 self.sensor_temp_01_new = self.sensor_temp_01_raw
                 self.sensor_temp_01_new.iloc[outlier_index, 2] = np.nan
                 self.sensor_temp_01_new[["value"]] = self.sensor_temp_01_new[["value"]].astype(float)
-                if(np.size(outlier_index)>0):
+                if(np.size(outlier_index) > 0):
                     self.sensor_temp_01_new[["value"]] = self.sensor_temp_01_new[["value"]].interpolate()
                 super().activate()
 
     def deltint(self):
-        self.sensor_temp_01_raw = pd.DataFrame(columns=["id","timestamp","value"])
-        self.sensor_temp_01_new = pd.DataFrame(columns=["id","timestamp","value"])
-        self.counter = 0
+        """Función DEVS de transición interna."""
+        # Reinicialización del dataframe temporal:
+        if(self.fusion_01_counter == self.n_samples):
+            self.fusion_01_raw = pd.DataFrame(columns=["id",
+                                                       "source",
+                                                       "datetime",
+                                                       "payload"])
+            self.fusion_01_counter = 0
+
+        # Reinicialización del dataframe temporal:
+        if(self.sensor_temp_01_counter == self.n_samples):
+            self.sensor_temp_01_raw = pd.DataFrame(columns=["id",
+                                                            "timestamp",
+                                                            "value"])
+            self.sensor_temp_01_new = pd.DataFrame(columns=["id",
+                                                            "timestamp",
+                                                            "value"])
+            self.sensor_temp_01_counter = 0
         self.passivate()
 
+
 class TestFogServer(Coupled):
+    """Clase para evaluar FogServer."""
+    def __init__(self, name, num_events=1000, n_samples=100):
+        """Inicialización de atributos."""
+        super().__init__(name)
+        gen = TmpGenerator("generator", num_events)
+        fog = FogServer("fog", n_samples)
+        self.add_component(gen)
+        self.add_component(fog)
+        self.add_coupling(gen.o_out, fog.i_sensor_temp_01)
 
-	def __init__(self, name, num_events=1000, n_samples=100):
-		super().__init__(name)
-
-		gen = TmpGenerator("generator", num_events)
-		fog = FogServer("fog", n_samples)
-
-		self.add_component(gen)
-		self.add_component(fog)
-
-		self.add_coupling(gen.o_out, fog.i_sensor_temp_01)
 
 if __name__ == '__main__':
     test_fog = TestFogServer("test_fog", 1000, 100)
--- models/prototypes.py
+++ models/prototypes.py
@@ -1,6 +1,8 @@
 """Clase que implementa algunos prototipos de modelos."""
 
 from xdevs.models import Coupled
+import datetime as dt
+from xdevs.sim import Coordinator
 from edge.file import FileIn, FussionPosBloom
 from fog.fog import FogServer
 from util.event import DataEventId
@@ -12,9 +14,9 @@ class Model_01(Coupled):
     def __init__(self, name, start, day, log=False):
         """Función de inicialización."""
         super().__init__(name)
-        file_ship = FileIn("ShipPos", "../data/LatLon"+day+".xlsx",
+        file_ship = FileIn("ShipPos", "data/LatLon"+day+".xlsx",
                            start=start, dataid=DataEventId.POS3D, log=log)
-        file_bloom = FileIn("DetBlo", "../data/DetBloom"+day+".xlsx",
+        file_bloom = FileIn("DetBlo", "data/DetBloom"+day+".xlsx",
                             start=start, dataid=DataEventId.BLOOM, log=log)
         # Fusiona Posición del barco con medida de Sensor de Bloom
         edge_fusion = FussionPosBloom("EdgeFussion")
@@ -27,3 +29,16 @@ class Model_01(Coupled):
         self.add_coupling(file_ship.o_out, edge_fusion.i_Pos)
         self.add_coupling(file_bloom.o_out, edge_fusion.i_Blo)
         self.add_coupling(edge_fusion.o_out, fog_server.i_fusion_01)
+
+
+def test():
+    """Comprobamos el funcionamiento de alguno de los modelos."""
+    day = "20210801"
+    start_dt = dt.datetime(2021, 8, 1, 0, 0, 0)
+    end_dt = dt.datetime(2021, 8, 2, 0, 0, 0)
+    sim_seconds = (end_dt-start_dt).total_seconds()
+    coupled = Model_01("day_" + day, start=start_dt, day=day, log=False)
+    coord = Coordinator(coupled)
+    coord.initialize()
+    coord.simulate_time(sim_seconds)
+    coord.exit()

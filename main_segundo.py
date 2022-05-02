e45d94e803b178eb2c05f362f7835a49bb396340
SEGUNDO ESTEBAN SAN ROMAN <Sesteban@ucm.es>
2022-05-02 11:48:47 +0200

    SimSensor ver3

--- main_segundo.py
+++ main_segundo.py
@@ -6,50 +6,66 @@ from xdevs.sim import Coordinator
 from xdevs.models import Coupled
 import datetime as dt
 
-from edge.file import FileIn,FileOut
-from edge.body import SimBody2
-from edge.sensor import SimSensor2
-from util.event import DataEventId,SimSenId
+from edge.sensor import SimSensor3
+from edge.file import FileOut,FileInVar
+from edge.body import SimBody3
+from util.event import SensorEventId,SensorInfo
 
-class Test1(Coupled):
-  '''Ejemplo acoplado que:
-    *desde un fichero pide TM de O2 a SimSensor
-    *SimSensor lee las TM de O2 desde un SimBody
-    *SimSensor genera la TM deO2 60s más tarde
-    *La TM de O2 se guardan en un fichero con t,lat,lon,dep
+class Test3(Coupled):
+  '''Ejemplo para utiliza SimBody3 y SimSensor3:
+    *Petición de TMs de O2, N2 y ALG generadas desde ficheros
+    *SimSensor3 se inicializa y manda su configuración
+    *SimSensor3 lee las TMs desde un SimBody3
+    *Se guardan TMs con t,lat,lon,depth y valor de la señal
   '''
-
   def __init__(self, name, simbody, start, log=False):
     super().__init__(name)
-    #AskSensor = FileIn("AskN", './data/LatLonDep.xlsx',start=start, dataid=SimSenId.NITROGEN, log=log)
-    AskSensor = FileIn("AskO", './data/LatLonDep.xlsx',start=start, dataid=SimSenId.OXIGEN, log=log)   
-    #Sensor = SimSensor("SimulatedSensor", simbody, delay=60, log=log)  
-    Sensor = SimSensor2("SimulatedSensor", simbody, start=start, delay=60, log=log)     
-    Outfile = FileOut("FileOutSimSen", './data/FileOutSensor.xlsx', log=log)     
-    self.add_component(AskSensor)
-    self.add_component(Sensor)
+    #Sensor parameters
+    Nseninf=SensorInfo(id=SensorEventId.NITROGEN,description="Sonda de Nitrogeno",delay=0.4, max= 0.5, min=0,precision=0.01,noisebias=0.001,noisesigma=0.001)
+    Oseninf=SensorInfo(id=SensorEventId.OXIGEN,description="Sonda de Oxigeno",delay=0.6, max= 10.0, min=0,precision=0.1,noisebias=0.01,noisesigma=0.01)
+    Aseninf=SensorInfo(id=SensorEventId.ALGA,description="Detector de Algas",delay=0.8, max= 0.01, min=0,precision=0.001,noisebias=0.001,noisesigma=0.001)
+    #Sensors telemetries resquet files
+    AskSensorN = FileInVar("Ask_N", './dataedge/Sweep2008_WQ_N.xlsx', start, dataid=SensorEventId.NITROGEN,  log=log)   
+    AskSensorO = FileInVar("Ask_O", './dataedge/Sweep2008_WQ_O.xlsx', start, dataid=SensorEventId.OXIGEN, log=log) 
+    AskSensorA = FileInVar("Ask_A", './dataedge/Sweep2008_WQ_ALG.xlsx', start, dataid=SensorEventId.ALGA, log=log) 
+    #Sensors models
+    SensorN = SimSensor3("SimSenN", simbody , Nseninf, log=log)       
+    SensorO = SimSensor3("SimSenO", simbody , Oseninf, log=log) 
+    SensorA = SimSensor3("SimSenA", simbody , Aseninf, log=log)     
+    Outfile = FileOut("Sensors2008Out", './dataedge/Sensors2008out2.xlsx', log=log)     
+    self.add_component(AskSensorN)
+    self.add_component(AskSensorO)
+    self.add_component(AskSensorA)
+    self.add_component(SensorN)
+    self.add_component(SensorO)
+    self.add_component(SensorA)
     self.add_component(Outfile)
-    self.add_coupling(AskSensor.o_out, Sensor.i_in)
-    self.add_coupling(Sensor.o_out, Outfile.i_in)
+    self.add_coupling(AskSensorN.o_out, SensorN.i_in)
+    self.add_coupling(AskSensorO.o_out, SensorO.i_in)
+    self.add_coupling(AskSensorA.o_out, SensorA.i_in)
+    self.add_coupling(SensorN.o_out, Outfile.i_in)
+    self.add_coupling(SensorO.o_out, Outfile.i_in)
+    self.add_coupling(SensorA.o_out, Outfile.i_in)
+        
+if __name__ == "__main__":
   
-
-startdt = dt.datetime(2021, 8, 1, 0, 0, 0)
-enddt = dt.datetime(2021, 8, 2, 0, 0, 0)
-simseconds = (enddt-startdt).total_seconds()
-
-print('Loading BodySim...')
-bodyfile = './body/Washington-1d-2008-09-12_compr.nc'
-#bodyfile= 'D:/Unidades compartidas/ia-ges-bloom-cm/IoT/Washington-1d-2008-09-12_compr.nc'
-vars = ('WQ_O', 'WQ_N')
-#simbody = SimBody('SimWater', bodyfile, vars)
-simbody = SimBody2('SimWater', bodyfile, vars)
-
-print('Loading Models...')
-coupled = Test1("SimBodyRead", simbody, startdt, True)
-coord = Coordinator(coupled, flatten=True)
-coord.initialize()
-
-print('Start Simulation.')
-coord.simulate_time(simseconds)  # En segundos
-coord.exit()
-print('End Simulation.')
+  #1 hour simulation to sweep the body (~2 min)
+  startdt = dt.datetime(2008,9,12,4,0,0)
+  enddt   = dt.datetime(2008,9,12,4,59,59)
+  simseconds=(enddt-startdt).total_seconds()
+  print('Sim IniDate:',startdt)
+  print('Sim EndDate:',enddt)
+  #print('BodySim loading...')
+  bodyfile = './body/Washington-1d-2008-09-12_compr.nc'
+  myvars=('WQ_O','WQ_N','WQ_ALG')
+  simbody=SimBody3('SimWater',bodyfile,myvars)
+  
+  print('Models Initialitation...')
+  coupled = Test3("SimBodyRead", simbody, startdt, log=False)
+  coord = Coordinator(coupled, flatten=True)
+  coord.initialize()
+  
+  print('Simulating...')
+  coord.simulate_time(simseconds)   
+  coord.exit()
+  print('End')
